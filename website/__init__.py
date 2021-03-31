45635d46dba2555b29fa230dd99324b872097591
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-03-31 19:59:40 +0900

    creating new user accounts

--- website/__init__.py
+++ website/__init__.py
@@ -1,22 +1,32 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = "databse.db"
 
+def create_database(app):
+    if not os.path.exists('website/'+DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
+
 def create_app():
     # __name__ represents name of file
+    from .views import views
+    from .auth import auth
+    from .models import User,Note
     app = Flask(__name__)
     # encrypts and secures cookies and session data
     # in ouur app.
     app.config['SECRET_KEY'] = 'rohit'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    from .views import views
-    from .auth import auth
     
     app.register_blueprint(views,url_prefix="/")
     app.register_blueprint(auth,url_prefix="/")
+    create_database(app)
+    
     
     return app
     
--- website/auth.py
+++ website/auth.py
@@ -3,8 +3,11 @@ auth.py contains the routes to
 all the pages in our web app
 that need authorization tokens.
 """
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from .models import User
+from . import db
 auth = Blueprint('auth',__name__)
 
 @auth.route("/login",methods=['GET','POST'])
@@ -15,20 +18,26 @@ def login():
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         if len(email)<5:
             flash("Email id should be more than 5 chars",category='error')
-        elif len(firstName) <=2:
+        elif len(first_name) <=2:
             flash("First name should be greater than 2 letters", category='error')
         elif password1 != password2:
             flash("The password doesnt match.",category='error')
         elif len(password1)<7:
             flash("The password must be atleast 7 characters.",category='error')
         else:
+            new_user = User(email=email,first_name=first_name,password=generate_password_hash(password1,method='sha256'))
+            # print("*"*100)
+            # print(new_user)
+            # print("*"*100)
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created',category='success')
-            
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
 
 @auth.route("/logout")
