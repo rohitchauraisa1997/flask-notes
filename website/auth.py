22e9337fff71e6cd7eddd0f63c7b445e6a4d8766
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-03-31 13:56:28 +0900

    passing variables into template and rendering them

--- website/auth.py
+++ website/auth.py
@@ -3,17 +3,17 @@ auth.py contains the routes to
 all the pages in our web app
 that need authorization tokens.
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html",text="testing",boolean=True)
 
 @auth.route("/sign_up")
 def signup():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
 
 @auth.route("/logout")
 def logout():
