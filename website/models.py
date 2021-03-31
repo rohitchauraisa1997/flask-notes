c6bd4f633d7041430632a9c8d80e43a808203ac0
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-03-31 17:56:01 +0900

    foreign key relationships

--- website/models.py
+++ website/models.py
@@ -7,9 +7,13 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    # when we define relationship we refer name of class.
+    notes = db.relationship('Note')
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) 
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True),default=func.now())
-    
+    # when we define relationship foreign key is lower case.
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
