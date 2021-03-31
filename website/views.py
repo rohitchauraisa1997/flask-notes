f9cf31351d065b77ea7476f50b598e35d222164c
Rohit Chaurasia <rohit.chaurasia997@gmail.com>
2021-03-31 13:07:32 +0900

    rendering first template on site with toggler

--- website/views.py
+++ website/views.py
@@ -2,11 +2,11 @@
 views.py contains the routes to 
 all the pages in our web app.
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return "<h1>test</h1>"
+    return render_template("home.html")
 
